62b31bbabce27fafb4c5adf57783d2858909222d
Esteban Ristich <esteban.ristich@protonmail.com>
2025-01-27 15:06:52 +0100

    feat: add application entrypoint

--- App.js
+++ App.js
@@ -1,33 +1,3 @@
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import BottomTabNavigator from './navigation/BottomTabNavigator';
-// import EventDetailsScreen from './screens/EventDetailsScreen';
-// import BookingScreen from './screens/BookingScreen';
-// import ConfirmationScreen from './screens/ConfirmationScreen';
-// import HomeScreen from './screens/HomeScreen';
-
-// const Stack = createNativeStackNavigator();
-
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator initialRouteName="Home">
-//         <Stack.Screen name="Home" component={HomeScreen} />
-//         <Stack.Screen 
-//           name="BottomTabs" 
-//           component={BottomTabNavigator} 
-//           options={{ headerShown: false }} 
-//         />
-//         <Stack.Screen name="EventDetails" component={EventDetailsScreen} />
-//         <Stack.Screen name="Booking" component={BookingScreen} />
-//         <Stack.Screen name="Confirmation" component={ConfirmationScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
-
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
